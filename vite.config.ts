71cf3afabc5cb2ad50bf639d0a62d23f42af94f8
Kevin Lagergren <kevlagergren@gmail.com>
2025-05-14 23:37:15 +0200

    fix firestore size chunk

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,11 @@ import react from '@vitejs/plugin-react';
 import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 
+
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
   const config:UserConfig = {
-    plugins: [react(), tailwindcss()],
+    plugins: [react(), tailwindcss() ],
     resolve: {
       alias: {
         '@': path.resolve(__dirname, './src'),
@@ -20,6 +21,25 @@ export default defineConfig(({ command }) => {
         '@services': path.resolve(__dirname, './src/services'),
         '@utils': path.resolve(__dirname, './src/utils'),
       },
+    },
+    // Fixes size issues with Firebase
+    build: { 
+      rollupOptions: {
+        output: {
+          manualChunks(id ) {
+            if (id.includes('node_modules/@firebase/') || id.includes('node_modules/firebase/')) {
+              return 'vendor-firebase';
+            }
+            if (id.includes('node_modules/react/') || id.includes('node_modules/react-dom/')) {
+              return 'vendor-react';
+            }
+            if (id.includes('node_modules')) {
+              return 'vendor'; 
+            }
+          },
+        },
+      },
+      chunkSizeWarningLimit: 700, 
     },
   };
 
