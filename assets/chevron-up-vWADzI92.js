6d01d8f98792b0d294e81225dc5aef32f8b6ea36
Kevin Lagergren <kevlagergren@gmail.com>
2025-05-20 14:30:11 +0200

    Updates

---
+++ assets/chevron-up-vWADzI92.js
@@ -0,0 +1,7 @@
+import{c as o}from"./index-CKYx6VMo.js";/**
+ * @license lucide-react v0.503.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const c=[["path",{d:"m18 15-6-6-6 6",key:"153udz"}]],n=o("chevron-up",c);export{n as C};
+//# sourceMappingURL=chevron-up-vWADzI92.js.map
