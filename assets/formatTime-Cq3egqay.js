5e7c283331c6cdd18efc7eb42892a0faa3417084
Kevin Lagergren <kevlagergren@gmail.com>
2025-05-20 14:23:20 +0200

    Updates

---
+++ assets/formatTime-Cq3egqay.js
@@ -0,0 +1,2 @@
+const i=t=>{if(t<60)return`${t}s`;const r=Math.floor(t/60);if(r<60)return`${r}m${t%60?` ${t%60}s`:""}`;const o=Math.floor(r/60),n=r%60;return`${o}h${n?` ${n}m`:""}`};export{i as f};
+//# sourceMappingURL=formatTime-Cq3egqay.js.map
