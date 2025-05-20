59b717306c67fca315bacaa64d958f96a1fdbc01
Kevin Lagergren <kevlagergren@gmail.com>
2025-05-20 14:31:48 +0200

    Updates

---
+++ assets/chevron-up-CMITYEf9.js
@@ -0,0 +1,7 @@
+import{r as o,c as u}from"./index-DL-4kom7.js";function n(r){const e=o.useRef({value:r,previous:r});return o.useMemo(()=>(e.current.value!==r&&(e.current.previous=e.current.value,e.current.value=r),e.current.previous),[r])}/**
+ * @license lucide-react v0.503.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const t=[["path",{d:"m18 15-6-6-6 6",key:"153udz"}]],s=u("chevron-up",t);export{s as C,n as u};
+//# sourceMappingURL=chevron-up-CMITYEf9.js.map
