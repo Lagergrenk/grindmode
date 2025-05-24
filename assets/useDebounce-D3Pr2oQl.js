d8ab67a589cbf0510d96b704e7896e8e3e489497
Kevin Lagergren <kevlagergren@gmail.com>
2025-05-24 20:17:51 +0200

    Updates

---
+++ assets/useDebounce-D3Pr2oQl.js
@@ -0,0 +1,7 @@
+import{c as n,r as c}from"./index-MKLLRtOd.js";/**
+ * @license lucide-react v0.503.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const u=[["path",{d:"M3 6h18",key:"d0wm0j"}],["path",{d:"M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6",key:"4alrt4"}],["path",{d:"M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2",key:"v07s0e"}],["line",{x1:"10",x2:"10",y1:"11",y2:"17",key:"1uufr5"}],["line",{x1:"14",x2:"14",y1:"11",y2:"17",key:"xtxkd"}]],d=n("trash-2",u);function i(e,t){const[o,r]=c.useState(e);return c.useEffect(()=>{const s=setTimeout(()=>{r(e)},t);return()=>{clearTimeout(s)}},[e,t]),o}export{d as T,i as u};
+//# sourceMappingURL=useDebounce-D3Pr2oQl.js.map
